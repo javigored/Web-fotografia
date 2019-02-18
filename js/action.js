cd24531e3dc934b611edd2f269a40bc35da82180
javigored <47102263+javigored@users.noreply.github.com>
2019-02-18 19:24:44 +0100

    Update action.js

--- js/action.js
+++ js/action.js
@@ -1,20 +1,67 @@
-$(document).ready(function() {
- 
-        var aboveHeight = $('header').outerHeight();
- 
-        $(window).scroll(function(){
- 
-                if ($(window).scrollTop() > aboveHeight){
- 
-                $('nav').addClass('fixed').css('top','0').next()
-                .css('padding-top','60px');
- 
-                } else {
- 
-                $('nav').removeClass('fixed').next()
-                .css('padding-top','0');
-                }
-        });
-});
+window.onload = function () {
+    const IMAGENES = [
+        'canon/natura1.jpg',
+        'canon/natura2.jpg',
+        'canon/urbano1.jpg'
+    ];
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 2000;
+    let posicionActual = 0;
+    let $botonRetroceder = document.querySelector('#retroceder');
+    let $botonAvanzar = document.querySelector('#avanzar');
+    let $imagen = document.querySelector('#imagen');
+    let $botonPlay = document.querySelector('#play');
+    let $botonStop = document.querySelector('#stop');
+    let intervalo;
+
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+
+
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+
+
+    function renderizarImagen () {
+        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+
+
+    function playIntervalo() {
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+        $botonAvanzar.setAttribute('disabled', true);
+        $botonRetroceder.setAttribute('disabled', true);
+        $botonPlay.setAttribute('disabled', true);
+        $botonStop.removeAttribute('disabled');
+
+    }
+
+
+    function stopIntervalo() {
+        clearInterval(intervalo);
+        $botonAvanzar.removeAttribute('disabled');
+        $botonRetroceder.removeAttribute('disabled');
+        $botonPlay.removeAttribute('disabled');
+        $botonStop.setAttribute('disabled', true);
+    }
+
+    $botonAvanzar.addEventListener('click', pasarFoto);
+    $botonRetroceder.addEventListener('click', retrocederFoto);
+    $botonPlay.addEventListener('click', playIntervalo);
+    $botonStop.addEventListener('click', stopIntervalo);
+    renderizarImagen();
+} 
+
 
 
